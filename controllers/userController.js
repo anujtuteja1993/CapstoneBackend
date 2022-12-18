bbe38bbab948906ef7d550062f9ede67a24c85e0
Anuj Tuteja <anujtuteja1993@gmail.com>
2022-12-19 01:30:22 +1300

    Fixed registerUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,7 @@ exports.registerUser = async (req, res) => {
     try {
         const { firstName, lastName, email, password } = req.body;
 
-        if(!email || !password || !firstName || !lastName) {
+        if (!email || !password || !firstName || !lastName) {
             return res.status(400).json({ success: false, msg: "One or more fields missing" });
         }
 
@@ -19,30 +19,37 @@ exports.registerUser = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, salt);
         const connection = mysql.createConnection(config);
         let sql = `INSERT INTO user (first_name, last_name, email, password) VALUES ('${firstName}', '${lastName}', '${email}', '${hashedPassword}')`;
-        connection.query(sql, (error, results, fields) => {
-            if (error) {
-                res.json(400);
-                res.json({ success: false, message: "The user already exists"});
-                //throw Error(error.message);
-            }
-            res.json({ success: true, message: "User successfully registered" });
+
+        let registerQueryResult = await new Promise((resolve, reject) => {
+            connection.query(sql, (error, results, fields) => {
+                if (error) {
+                    reject(new Error("Email  already exists"));
+                }
+                resolve({
+                    message: "User Created Successfully",
+                    success: true,
+                    data: results,
+                });
+            });
         });
 
         connection.end();
+        res.status(200);
+        res.json({ success: true, message: "User successfully registered" });
     }
-    catch (e) {
+    catch (error) {
         res.status(400);
-        res.json({ success: false, message: e.message });
-        throw Error(e.message);
+        res.json({ success: false, message: error.message });
     }
 };
+
 
 exports.userLogin = async (req, res) => {
     try {
 
         const { email, password } = req.body;
 
-        if(!email || !password) {
+        if (!email || !password) {
             return res.status(400).json({ success: false, msg: "Email or Password missing" });
         }
 
@@ -71,22 +78,22 @@ exports.userLogin = async (req, res) => {
         const id = emailQueryResult.data[0].id;
         const firstName = emailQueryResult.data[0].first_name;
         const lastName = emailQueryResult.data[0].last_name;
-        const loginuseremail = emailQueryResult.data[0].email;
+        const loginUserEmail = emailQueryResult.data[0].email;
         const accessToken = jwt.sign(
-            { id, firstName, lastName, loginuseremail },
+            { id, firstName, lastName, loginUserEmail },
             process.env.ACCESS_TOKEN_SECRET,
             {
                 expiresIn: "1d",
             }
         );
         const refreshToken = jwt.sign(
-            { id, firstName, lastName, loginuseremail },
+            { id, firstName, lastName, loginUserEmail },
             process.env.REFRESH_TOKEN_SECRET,
             {
                 expiresIn: "1d",
             }
         );
-        
+
         let emailQueryUpdate = await new Promise((resolve, reject) => {
             const connection = mysql.createConnection(config);
             let sql = `UPDATE user SET refresh_token = "${refreshToken}" WHERE id = "${id}";`;
@@ -110,53 +117,55 @@ exports.userLogin = async (req, res) => {
         res.status(200);
         res.json({ success: true, msg: "Log in successful", token: accessToken });
     } catch (error) {
-        console.log(error);
         res.status(404).json({ msg: "Email or Password is incorrect." });
     }
 };
 
 exports.userLogout = async (req, res) => {
-    const { id } = req.body;
+    try {
+        const { email } = req.body;
 
-    const connection = mysql.createConnection(config);
-    let sql = `SELECT * FROM user WHERE id = "${id}";`;
-
-    let userQuery = await new Promise((resolve, reject) => {
-        connection.query(sql, (error, results, fields) => {
-            if (error) {
-                reject(new Error("Unable to get query by token or id"));
-            }
-
-            resolve({
-                message: "successful",
-                success: true,
-                data: results,
-            });
-        });
-    });
-
-    connection.end();
-
-    if (!id) return res.sendStatus(204);
-
-    let clearRefreshtoken = await new Promise((resolve, reject) => {
         const connection = mysql.createConnection(config);
-        let sql = `UPDATE user SET refresh_token = NULL WHERE id = ${id};`;
-        connection.query(sql, (error, results, fields) => {
-            if (error) {
-                reject(new Error("Logout Error"));
-            }
-            resolve({
-                message: "Logout Successful",
-                success: true,
-                data: results,
+        let sql = `SELECT * FROM user WHERE email = "${email}";`;
+        let userQuery = await new Promise((resolve, reject) => {
+            connection.query(sql, (error, results, fields) => {
+                if (error) {
+                    reject(new Error("Unable to get query by token or id"));
+                }
+
+                resolve({
+                    message: "successful",
+                    success: true,
+                    data: results,
+                });
             });
         });
-        connection.end();
-    });
 
-    res.status(200);
-    res.json({ success: true, msg: "Logout successful" });
+        connection.end();
+
+        if (!email) return res.sendStatus(204);
+
+        let clearRefreshtoken = await new Promise((resolve, reject) => {
+            const connection = mysql.createConnection(config);
+            let sql = `UPDATE user SET refresh_token = NULL WHERE email = "${email}";`;
+            connection.query(sql, (error, results, fields) => {
+                if (error) {
+                    reject(new Error("Logout Error"));
+                }
+                resolve({
+                    message: "Logout Successful",
+                    success: true,
+                    data: results,
+                });
+            });
+            connection.end();
+        });
+
+        res.status(200);
+        res.json({ success: true, msg: "Logout successful" });
+    } catch (error) {
+        res.status(404).json({ msg: "Logout failed." });
+    }
 };
 
 exports.getUserDetails = async (req, res) => {
@@ -169,7 +178,7 @@ exports.getUserDetails = async (req, res) => {
             if (error) {
                 throw Error(error.message);
             }
-            res.json({ success: true, message: "User Details found", data: results});
+            res.json({ success: true, message: "User Details found", data: results });
         });
 
         connection.end();
@@ -193,7 +202,7 @@ exports.updateUserDetails = async (req, res) => {
             if (error) {
                 throw Error(error.message);
             }
-            res.json({ success: true, message: "User has been updated", data: results});
+            res.json({ success: true, message: "User has been updated", data: results });
         });
 
         connection.end();
@@ -216,7 +225,7 @@ exports.deleteUser = async (req, res) => {
             if (error) {
                 throw Error(error.message);
             }
-            res.json({ success: true, message: "User has been deleted", data: results});
+            res.json({ success: true, message: "User has been deleted", data: results });
         });
 
         connection.end();
